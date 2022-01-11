4e8f96c634ca9693d7ab748465d713a0e2d5340e
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-12 04:33:24 +0500

    Add agregated functions

--- queries.sql
+++ queries.sql
@@ -9,3 +9,10 @@ SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.40 AND 17.30 OR weight_kg = 10.40 AND weight_kg = 17.30;
 
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals GROUP BY escape_attempts HAVING escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01'  AND '2000-12-31' GROUP BY species;
